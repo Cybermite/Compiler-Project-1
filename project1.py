e3345549d9fc520ab8343aa76a6be2990c0d3880
zralston <z641r311@ku.edu>
2014-09-29 18:21:50 -0500

    Added the E-Closure function.

--- project1.py
+++ project1.py
@@ -10,13 +10,20 @@ def separate_states(state_list):
 
     return states
 
+def e_closure(state_list, states):
+    e_closure_states = set(state_list)
+    for state in state_list:
+        e_closure_states = e_closure_states | set(e_closure(states[state-1]['E'], states))
+
+    return list(e_closure_states)
+
 if __name__ == '__main__':
     # Used to allow this to be a standalone program and not run this code if
     # it's imported from somewhere else.
 
     # interate over the lines from standard in.
 
-    initial_state = separate_states(stdin.readline().split(':')[1])[0]
+    initial_state = separate_states(stdin.readline().split(':')[1])
     final_states = separate_states(stdin.readline().split(':')[1])
     state_count = separate_states(stdin.readline().split(':')[1])[0] 
 
@@ -31,19 +38,17 @@ if __name__ == '__main__':
 
     states = []
 
-    for i in range(1, int(state_count)+1):
+    for i in range(0, state_count):
         states.append({})
         line = stdin.readline().split('{')
         
         # get rid of the state number
         line.pop(0)
         for token in tokens:
-            states[0][token] = separate_states(line[0])
+            states[i][token] = separate_states(line[0])
             line.pop(0)
-            print(states[0][token], end='')
+            print(states[i][token], end='')
         
         print('')
-
-
-#    for line in stdin:
- #       print(line, end='')
+    
+    # print(e_closure(initial_state, states))
