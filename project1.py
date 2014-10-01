864054a9886256e0d76a95a86e1c8911c933e26f
zralston <z641r311@ku.edu>
2014-10-01 13:50:56 -0500

    Added converting from nfa to dfa, but the outputing is a little bad atm.

--- project1.py
+++ project1.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from sys import stdin
+import pprint
 
 
 def separate_states(state_list):
@@ -19,6 +20,64 @@ def e_closure(state_list, states):
         e_closure_states = e_closure_states | my_set
 
     return list(e_closure_states)
+
+
+def move(state_list, states, symbol):
+    set_states = set()
+    for state in state_list:
+        # print(states[state][symbol])
+        my_set = set(states[state][symbol])
+        set_states = set_states | my_set
+
+    return list(set_states)
+
+
+def find_index(state_list, d_states):
+    for i in range(0, len(d_states)):
+        if set(d_states[i]) == set(state_list):
+            return i
+
+    return len(d_states)
+
+
+def convert_to_dfa(io, states, tokens):
+    d_states = []
+    dfa_states = {}
+    d_states.append(e_closure(io, states))
+    marked_index = 0
+    while marked_index < len(d_states):
+        print('')
+        state_list = d_states[marked_index]
+        marked_index += 1
+        print("Mark " + str(marked_index))
+        dfa_states[marked_index] = {}
+        for token in tokens:
+            if token != 'E':
+                dfa_states[marked_index][token] = []
+                states_on_token = move(state_list, states, token)
+                if len(states_on_token) > 0:
+                    print(state_list, end='')
+                    print(" --" + token + "--> ", end='')
+                    print(states_on_token)
+                    print("E-closure", end='')
+                    print(states_on_token, end='')
+                    print(" = ", end='')
+                    u = e_closure(states_on_token, states)
+                    print(u, end='')
+                    print(" = ", end='')
+
+                    index_of_state = find_index(u, d_states)
+                    if index_of_state == len(d_states):
+                        # if it is a new state not on d_states
+                        d_states.append(u)
+
+                    print(index_of_state+1)
+                    dfa_states[marked_index][token] = index_of_state+1
+
+            # print(u)
+    # print(d_states)
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(dfa_states)
 
 
 if __name__ == '__main__':
@@ -41,7 +100,6 @@ if __name__ == '__main__':
     print(tokens)
 
     states = {}
-
     for i in range(0, state_count):
         line = stdin.readline().split('{')
         state_name = line.pop(0).strip()
@@ -54,5 +112,7 @@ if __name__ == '__main__':
 
         print('')
 
-    # print(states)
-    print(e_closure(e_closure(initial_state, states), states))
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(states)
+    convert_to_dfa(initial_state, states, tokens)
+    # print(e_closure(e_closure(initial_state, states), states))
