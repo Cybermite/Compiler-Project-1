373138694095b807fa8431886d56dc41de11c83a
zralston <z641r311@ku.edu>
2014-10-01 22:23:40 -0500

    Finished the convert_to_dfa function. Added function documentation to all the functions.

--- project1.py
+++ project1.py
@@ -1,9 +1,13 @@
 from __future__ import print_function
 from sys import stdin
-import pprint
 
 
 def separate_states(state_list):
+    """
+    Summary: Will return a list of states that is inclosed within the '{}'
+    state_list: The state list that is of the format '{number, number, ...}'
+    Return: Returns a list of the seprate states
+    """
     state_list = state_list.strip().strip('{}')
     states = []
     for state in state_list.split(','):
@@ -14,8 +18,16 @@ def separate_states(state_list):
 
 
 def e_closure(state_list, states):
+    """
+    Summary: Will do the null closure from each state within state_list
+    state_list: list of states that is apart of one bigger state in the diagram
+    states: The entire nfa transition dictionary
+    Return: Returns a list of states that can be reachable from this state_list
+            on 'E' transition
+    """
     e_closure_states = set(state_list)
     for state in state_list:
+        # check to see if E exist
         my_set = set(e_closure(states[state]['E'], states))
         e_closure_states = e_closure_states | my_set
 
@@ -23,6 +35,13 @@ def e_closure(state_list, states):
 
 
 def move(state_list, states, symbol):
+    """
+    Summary: Will do the move function from this list of states on the symbol.
+    state_list: current list of states
+    states: THe entire nfa transition dictionary
+    Return: Returns a list of states that are reachable from this state_list on
+            the symbol passed in.
+    """
     set_states = set()
     for state in state_list:
         # print(states[state][symbol])
@@ -33,6 +52,14 @@ def move(state_list, states, symbol):
 
 
 def find_index(state_list, d_states):
+    """
+    Summary: Will search through the list of d_states to see if the current
+            state is already in d_states
+    state_list: current list of states
+    d_states: The new states currently after eliminating 'E' transitions
+    Return: Returns the index of the this state within d_states. If the state
+            wasn't in d_states, it will return the length of d_states.
+    """
     for i in range(0, len(d_states)):
         if set(d_states[i]) == set(state_list):
             return i
@@ -40,51 +67,128 @@ def find_index(state_list, d_states):
     return len(d_states)
 
 
-def convert_to_dfa(io, states, tokens):
+def print_state(state_list):
+    """
+    Summary: Will print out the current state between brackets ({values})
+    state_list: current list of states to print out
+    Return: none
+    """
+    print('{', end='')
+    for i in range(0, len(state_list)):
+        print('%s' % (state_list[i]), end='')
+        if i != len(state_list)-1:
+            # if it's not the last state in this list
+            print(',', end='')
+
+    print('}', end='')
+
+
+def is_final_state(state_list, final_states):
+    """
+    Summary: Checks to see if this state is suppose to be a final state
+    state_list: The state, which can have multiple states apart of it.
+    final_states: The list of final states before the dfa conversion.
+    Return: Returns true if it's suppose to be a final state, false otherwise.
+    """
+    for state in state_list:
+        for final_state in final_states:
+            if state == final_state:
+                return True
+
+    return False
+
+
+def print_dfa(states, tokens, final_states, initial_state):
+    """
+    Summary: Prints out the dfa
+    states: Dictionary of my states and transitions
+    tokens: symbols that is apart of the language
+    final_states: list of final states
+    initial_state: list of initial state
+    Return: none
+    """
+    print('Initial State: ', end='')
+    print_state(initial_state)
+    print('')
+    print('Final States: ', end='')
+    print_state(final_states)
+    print('')
+    print('State\t', end='')
+
+    for token in tokens:
+        if token != 'E':
+            print('%s\t' % (token), end='')
+
+    print('')
+    for state in states:
+        print('%d\t' % (state), end='')
+        for token in tokens:
+            if token != 'E':
+                print_state(states[state][token])
+                print('\t', end='')
+
+        print('')
+
+
+def convert_to_dfa(io, states, tokens, final_states):
+    """
+    Summary: Will convert the nfa to a dfa
+    io: list of the initial state
+    states: Dictionary of my states and transitions on symbols
+    tokens: List of symbols that moves can happen on.
+    final_states: List of final states
+    Return: none
+    """
     d_states = []
+    dfa_final_states = []
+    dfa_initial_state = []
     dfa_states = {}
     d_states.append(e_closure(io, states))
     marked_index = 0
+    print('E-closure(I0) = ', end='')
+    print_state(d_states[0])
+    print(' = %d' % (marked_index+1))
+    dfa_initial_state.append(marked_index+1)
     while marked_index < len(d_states):
         print('')
         state_list = d_states[marked_index]
         marked_index += 1
-        print("Mark " + str(marked_index))
+        print('Mark %d' % (marked_index))
         dfa_states[marked_index] = {}
         for token in tokens:
             if token != 'E':
                 dfa_states[marked_index][token] = []
                 states_on_token = move(state_list, states, token)
                 if len(states_on_token) > 0:
-                    print(state_list, end='')
-                    print(" --" + token + "--> ", end='')
-                    print(states_on_token)
-                    print("E-closure", end='')
-                    print(states_on_token, end='')
-                    print(" = ", end='')
+                    print_state(state_list)
+                    print(' --' + token + '--> ', end='')
+                    print_state(states_on_token)
+                    print('')
+                    print('E-closure', end='')
+                    print_state(states_on_token)
+                    print(' = ', end='')
                     u = e_closure(states_on_token, states)
-                    print(u, end='')
-                    print(" = ", end='')
+
+                    print_state(u)
+                    print(' = ', end='')
 
                     index_of_state = find_index(u, d_states)
                     if index_of_state == len(d_states):
                         # if it is a new state not on d_states
                         d_states.append(u)
+                        if is_final_state(u, final_states):
+                            dfa_final_states.append(index_of_state+1)
 
                     print(index_of_state+1)
-                    dfa_states[marked_index][token] = index_of_state+1
+                    dfa_states[marked_index][token].append(index_of_state+1)
 
-            # print(u)
-    # print(d_states)
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(dfa_states)
+    print('')
+    print_dfa(dfa_states, tokens, dfa_final_states, dfa_initial_state)
 
 
 if __name__ == '__main__':
     # Used to allow this to be a standalone program and not run this code if
     # it's imported from somewhere else.
-
-    # interate over the lines from standard in.
 
     initial_state = separate_states(stdin.readline().split(':')[1])
     final_states = separate_states(stdin.readline().split(':')[1])
@@ -93,11 +197,6 @@ if __name__ == '__main__':
     # split on whitespace with no arguments to split
     tokens = stdin.readline().strip().split()
     tokens.pop(0)
-
-    print(initial_state)
-    print(final_states)
-    print(state_count)
-    print(tokens)
 
     states = {}
     for i in range(0, state_count):
@@ -108,11 +207,5 @@ if __name__ == '__main__':
         for token in tokens:
             states[state_name][token] = separate_states(line[0])
             line.pop(0)
-            print(states[state_name][token], end='')
 
-        print('')
-
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(states)
-    convert_to_dfa(initial_state, states, tokens)
-    # print(e_closure(e_closure(initial_state, states), states))
+    convert_to_dfa(initial_state, states, tokens, final_states)
